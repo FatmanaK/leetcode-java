e23bd2767bd7aa79710e8842660040cb4f7e919d
Vineet John <vinjohn10@gmail.com>
2017-09-03 14:23:20 -0400

    Added optimized solution for 3sum

--- 3sum/src/ca/uwaterloo/Solution.java
+++ 3sum/src/ca/uwaterloo/Solution.java
@@ -1,84 +1,48 @@
 package ca.uwaterloo;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class Solution {
 
-    public class IndexTuple {
-        int x;
-        int y;
-
-        public IndexTuple(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public String toString() {
-            return "IndexTuple{" +
-                    "x=" + x +
-                    ", y=" + y +
-                    '}';
-        }
-    }
-
-    public class ValueTriple {
-        List<Integer> triplet;
-
-        public ValueTriple(int x, int y, int z) {
-            triplet = new ArrayList<>();
-            triplet.add(x);
-            triplet.add(y);
-            triplet.add(z);
-            Collections.sort(triplet);
-        }
-
-        @Override
-        public int hashCode() {
-            String hashString =
-                String.valueOf(triplet.get(0)) + String.valueOf(triplet.get(1)) + String.valueOf(triplet.get(2));
-            return hashString.hashCode();
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            return o.hashCode() == this.hashCode();
-        }
-
-        @Override
-        public String toString() {
-            return "ValueTriple{" +
-                    "triplet=" + triplet +
-                    '}';
-        }
-    }
-
     public List<List<Integer>> threeSum(int[] nums) {
-        HashMap<IndexTuple, Integer> sumTracker = new HashMap<>();
+        List<List<Integer>> candidates = new ArrayList<>();
+        Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                IndexTuple key = new IndexTuple(i, j);
-                sumTracker.put(key, nums[i] + nums[j]);
+        for (int firstIndex = 0; firstIndex < nums.length - 2; firstIndex++) {
+            int secondIndex = firstIndex + 1;
+            int thirdIndex = nums.length - 1;
+
+            if (firstIndex > 0 && (nums[firstIndex] == nums[firstIndex - 1])) {
+                continue;
             }
-        }
 
-//        System.out.println(sumTracker);
-        HashSet<ValueTriple> finalCandidates = new HashSet<>();
+            while (secondIndex < thirdIndex) {
+                int sum = nums[firstIndex] + nums[secondIndex] + nums[thirdIndex];
+                if (sum == 0) {
+                    candidates.add(
+                        Arrays.asList(nums[firstIndex], nums[secondIndex],
+                            nums[thirdIndex]));
 
-        for (int i = 0; i < nums.length; i++) {
-            for (IndexTuple tuple: sumTracker.keySet()) {
-                if (tuple.x != i && tuple.y != i && nums[i] + sumTracker.get(tuple) == 0) {
-                    finalCandidates.add(new ValueTriple(nums[tuple.x], nums[tuple.y], nums[i]));
+                    while (secondIndex < thirdIndex &&
+                        nums[secondIndex] == nums[secondIndex + 1]) {
+                        ++secondIndex;
+                    }
+                    while (secondIndex < thirdIndex &&
+                        nums[thirdIndex] == nums[thirdIndex - 1]) {
+                        --thirdIndex;
+                    }
+
+                    ++secondIndex;
+                    --thirdIndex;
+                } else if (sum > 0) {
+                    --thirdIndex;
+                } else {
+                    ++secondIndex;
                 }
             }
         }
-
-        List<List<Integer>> candidates = new ArrayList<>();
-        finalCandidates.forEach(
-            triple -> candidates.add(triple.triplet)
-        );
-//        System.out.println(candidates);
 
         return candidates;
     }
